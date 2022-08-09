96ed06b7514a71e5783ce0710d4e59752a3886ae
harry <harry7801234@gmail.com>
2022-08-09 12:23:28 -0600

    borrador

--- Cargar_archivo.py
+++ Cargar_archivo.py
@@ -1,20 +1,47 @@
+from curso_model import Curso
+from gestionar_curso import DB
+
 class CargarArchivo:
-    def __init__(self, codigo, nombre, prerrequisito, obligatorio):
-        self.codigo = codigo
-        self.nombre = nombre
-        self.prerrequisito = prerrequisito
-        self.obligatorio = obligatorio
+    lista = []
+    sulist = []
 
-    # GETTERS
-    def get_codigo(self):
-        return self.codigo  
+    with open("entrada.LFP", "r") as archivo:
+        lines = archivo.readlines()
+        print(lines)
+        #for linea in archivo:
+            #lista.append(linea.rstrip())
+            #print(linea)
 
-    def get_nombre(self):
-        return self.nombre
+    print("-------------------")
+    for line in lines:
+        print(line.rstrip())
 
-    def get_prerrequisito(self):
-        return self.prerrequisito
+        lista.append(line.split(","))
+        
+    print(".......................")
+    #print("letra dentro de la lista")
+    #separacion = lista[3][4]
+    #print(separacion)
+    print(lista)
+    print(".......................")
+    print(lista[0][0])#codigo
+    codigo =lista[0][0]
+    print(lista[0][1])#nombre
+    nombre = lista[0][1]
+    print(lista[0][2])#prerequisito
+    prerequisito = lista[0][2]
+    print(lista[0][3])#obligatorio
+    obligatorio = lista[0][3]
+    print(lista[0][4])#semestre
+    semestre = lista[0][4]
+    print(lista[0][5])#creditos
+    creditos = lista[0][5]
+    print(lista[0][6])#estado
+    estado = lista[0][6]
 
-    def get_obligatorio(self):
-        return self.obligatorio
+    objeto = Curso(codigo, nombre, prerequisito, obligatorio, semestre,
+    creditos, estado)
+    DB.imprimir_nombre(objeto)
 
+    #print("imprimir linea entera en una lista")
+    #print(lista[3].split(","))
